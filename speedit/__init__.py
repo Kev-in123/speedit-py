919d859aef4ee578603c74d667ae79b5b24077a3
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-09-25 20:31:35 -0400

    Update __init__.py

--- speedit/__init__.py
+++ speedit/__init__.py
@@ -2,15 +2,18 @@ __author__ = "Kev-in123"
 __title__ = "speedit-py"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Kev-in123"
-__version__ = "1.0"
+__version__ = "1.1"
 
-import time
+from time import perf_counter
+from asyncio import iscoroutinefunction, run
 
 def speed(func):
   def wrapper(*args, **kwargs):
-      start = time.perf_counter()
-      func(*args, **kwargs)
-      end = time.perf_counter()
+      start = perf_counter()
+      if iscoroutinefunction(func):
+        run(func(*args, **kwargs))
+      else:
+        func(*args, **kwargs)
+      end = perf_counter()
       print(f"Function \"{func.__name__}\" took {end-start} seconds to complete")
   return wrapper
-
